ebb51557d60b57df4443dff24607e250fc95fe19
tai <64537144+huynhtritaiml2@users.noreply.github.com>
2021-06-24 16:32:33 +0700

    Update bash.sh

--- languages/bash.sh
+++ languages/bash.sh
@@ -80,7 +80,8 @@ clear               # clears content on window (hide displayed lines)
 
 
 ls                            # lists your files in current directory, ls <dir> to print files in a specific directory
-ls -l                         # lists your files in 'long format', which contains the exact size of the file, who owns the file and who has the right to look at it, and when it was last modified
+ls -l                         # lists your files in 'long format', which contains the exact size of the file, who owns the file and who has the right 
+                              # to look at it, and when it was last modified
 ls -a                         # lists all files in 'long format', including hidden files (name beginning with '.')
 ln -s <filename> <link>       # creates symbolic link to file
 readlink <filename>           # shows where a symbolic links points to
@@ -94,12 +95,14 @@ more <filename>               # shows the first part of a file (move with space 
 head <filename>               # outputs the first lines of file (default: 10 lines)
 tail <filename>               # outputs the last lines of file (useful with -f option) (default: 10 lines)
 vim <filename>                # opens a file in VIM (VI iMproved) text editor, will create it if it doesn't exist
-mv <filename1> <dest>         # moves a file to destination, behavior will change based on 'dest' type (dir: file is placed into dir; file: file will replace dest (tip: useful for renaming))
+mv <filename1> <dest>         # moves a file to destination, behavior will change based on 'dest' type (dir: file is placed into dir; file: file will replace 
+                              # dest (tip: useful for renaming))
 cp <filename1> <dest>         # copies a file
 rm <filename>                 # removes a file
 find . -name <name> <type>    # searches for a file or a directory in the current directory and all its sub-directories by its name
 diff <filename1> <filename2>  # compares files, and shows where they differ
-wc <filename>                 # tells you how many lines, words and characters there are in a file. Use -lwc (lines, word, character) to ouput only 1 of those informations
+wc <filename>                 # tells you how many lines, words and characters there are in a file. Use -lwc (lines, word, character) to ouput only 1 of those 
+                              # informations
 sort <filename>               # sorts the contents of a text file line by line in alphabetical order, use -n for numeric sort and -r for reversing order.
 sort -t -k <filename>         # sorts the contents on specific sort key field starting from 1, using the field separator t.
 rev                           # reverse string characters (hello becomes olleh)
@@ -212,7 +215,8 @@ declare -x                   # marks the variables for export via the environmen
 ${varname:-word}             # if varname exists and isn't null, return its value; otherwise return word
 ${varname:word}              # if varname exists and isn't null, return its value; otherwise return word
 ${varname:=word}             # if varname exists and isn't null, return its value; otherwise set it word and then return its value
-${varname:?message}          # if varname exists and isn't null, return its value; otherwise print varname, followed by message and abort the current command or script
+${varname:?message}          # if varname exists and isn't null, return its value; otherwise print varname, followed by message and abort the current command 
+                             # or script
 ${varname:+word}             # if varname exists and isn't null, return word; otherwise return null
 ${varname:offset:length}     # performs substring expansion. It returns the substring of $varname starting at offset and up to length characters
 
